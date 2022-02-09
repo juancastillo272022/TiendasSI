38151c4c7c4489d1d8459154e1d71a624c42c318
juancastillo272022 <99351957+juancastillo272022@users.noreply.github.com>
2022-02-09 18:09:53 -0500

    carga del código fuente

---
+++ TiendasSI.Api/TiendasSI.Api/Controllers/ProductionController.cs
@@ -0,0 +1,103 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TiendasSI.Api.Models;
+
+namespace TiendasSI.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductionController : ControllerBase
+    {
+        private readonly TiendasSIContext _context = new();
+
+        // GET: api/Production
+        [HttpGet]
+        public async Task<ActionResult<List<Production>>> GetProductions()
+        {
+            return await _context.Productions.ToListAsync();
+        }
+
+        // GET: api/Production/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Production>> GetProduction(int id)
+        {
+            var production = await _context.Productions.FindAsync(id);
+
+            if (production == null)
+            {
+                return NotFound();
+            }
+
+            return production;
+        }
+
+        // PUT: api/Production/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduction(int id, Production production)
+        {
+            if (id != production.IdProduction)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(production).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Production
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Production>> PostProduction(Production production)
+        {
+            _context.Productions.Add(production);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduction", new { id = production.IdProduction }, production);
+        }
+
+        // DELETE: api/Production/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduction(int id)
+        {
+            var production = await _context.Productions.FindAsync(id);
+            if (production == null)
+            {
+                return NotFound();
+            }
+
+            _context.Productions.Remove(production);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductionExists(int id)
+        {
+            return _context.Productions.Any(e => e.IdProduction == id);
+        }
+    }
+}
---
+++ TiendasSI.Api/TiendasSI.Api/Models/Production.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TiendasSI.Api.Models
+{
+    public partial class Production
+    {
+        public int IdProduction { get; set; }
+        public string Title { get; set; } = null!;
+        public DateTime ReleaseDate { get; set; }
+        public string Producer { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public decimal Value { get; set; }
+        public bool Status { get; set; }
+    }
+}
